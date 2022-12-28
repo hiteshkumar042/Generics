71ad7b2b90a776855128705221f82bbff58d6123
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-29 00:23:45 +0530

    UC1 Added to PrintArray

---
+++ Generics/PrintArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] inputtArray)
+        {
+            Console.WriteLine("Int Array : ");
+            foreach(var intData in inputtArray)
+            {
+                Console.Write(intData+" ");
+            }
+        }
+        public static void ToPrint(double[] inputtArray)
+        {
+            Console.WriteLine("\nDouble Array : ");
+            foreach (var doubleData in inputtArray)
+            {
+                Console.Write(doubleData+" ");
+            }
+        }
+        public static void ToPrint(char[] inputtArray)
+        {
+            Console.WriteLine("\nChar Array : ");
+            foreach (var charData in inputtArray)
+            {
+                Console.Write(charData+" ");
+            }
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Generics");
+            int[] intArray = { 42, 38, 76 };
+            PrintArray.ToPrint(intArray);
+            double[] doubleArray = { 32.4, 23.6, 12.9 };
+            PrintArray.ToPrint(doubleArray);
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            PrintArray.ToPrint(charArray);
         }
     }
 }
